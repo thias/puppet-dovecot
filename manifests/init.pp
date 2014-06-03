f3b256a4599ec78381d579b50f533e60308b2608
Chris Malton <chris@vostron.com>
2014-06-03 10:45:22 +0100

    Add missing inherits directive.

--- manifests/init.pp
+++ manifests/init.pp
@@ -49,7 +49,7 @@ class dovecot (
   # auth-sql.conf.ext
   $auth_sql_userdb_static     = undef
 
-) {
+) inherits ::dovecot::params {
 
   # All files in this scope are dovecot configuration files
   File {
